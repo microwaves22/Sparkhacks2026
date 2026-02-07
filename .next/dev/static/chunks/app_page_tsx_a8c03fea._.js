e2785ecc7e1c111e4e408b9d442e42661c9d3e70
Michelle Ye <mye21@uic.edu>
2026-02-07 15:16:22 -0600

    edit

--- .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
+++ .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
@@ -4,11 +4,10 @@
   "static/chunks/node_modules_three_build_three_core_996ef05a.js",
   "static/chunks/node_modules_three_build_three_module_0c59ee63.js",
   "static/chunks/node_modules_three_build_three_module_f7d46d9b.js",
-  "static/chunks/node_modules_@react-three_fiber_dist_906c7f14._.js",
-  "static/chunks/node_modules_three-stdlib_1f08e6ce._.js",
-  "static/chunks/node_modules_@react-spring_core_dist_react-spring_core_modern_mjs_6c051fc7._.js",
-  "static/chunks/node_modules_c434c715._.js",
-  "static/chunks/app_components_495470dd._.js"
+  "static/chunks/node_modules_@react-three_fiber_dist_79ea1cf7._.js",
+  "static/chunks/node_modules_three-stdlib_f467cc33._.js",
+  "static/chunks/node_modules_8d940eb7._.js",
+  "static/chunks/app_components_69cc75d3._.js"
 ],
     source: "dynamic"
 });
